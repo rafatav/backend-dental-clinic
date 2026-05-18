a394c337ad66f9c5773e0ff6f108e9d6ef66e194
Rafael Tavares <tavares.rfl@gmail.com>
2026-05-18 10:33:37 -0300

    chore: initialize project structure

---
+++ src/test/java/com/wise/dental_clinic/DentalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wise.dental_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
