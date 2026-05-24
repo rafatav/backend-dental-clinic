c73a381d0abbf86eb5ecc32df6fdb4e7f1cca3e3
Rafael Tavares <tavares.rfl@gmail.com>
2026-05-24 17:52:01 -0300

    refactor: add optional @repository annotation

--- src/main/java/com/wise/dental_clinic/repositories/AppointmentRepository.java
+++ src/main/java/com/wise/dental_clinic/repositories/AppointmentRepository.java
@@ -2,6 +2,8 @@ package com.wise.dental_clinic.repositories;
 
 import com.wise.dental_clinic.entities.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 }
