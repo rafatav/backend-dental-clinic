43398e8354adcbe0be902fbeba2d83d7794ae2da
Rafael Tavares <tavares.rfl@gmail.com>
2026-06-01 14:42:18 -0300

    style: remove .* imports

--- src/main/java/com/wise/dental_clinic/entities/Appointment.java
+++ src/main/java/com/wise/dental_clinic/entities/Appointment.java
@@ -1,6 +1,15 @@
 package com.wise.dental_clinic.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
