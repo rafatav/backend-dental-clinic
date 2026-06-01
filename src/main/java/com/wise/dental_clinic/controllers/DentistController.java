55e377494b2693613a2b7950b1dc896e97141d36
Rafael Tavares <tavares.rfl@gmail.com>
2026-06-01 18:14:10 -0300

    feat: add DentistController
    add dentist controller with base methods

---
+++ src/main/java/com/wise/dental_clinic/controllers/DentistController.java
@@ -0,0 +1,59 @@
+package com.wise.dental_clinic.controllers;
+
+import com.wise.dental_clinic.dto.DentistDTO;
+import com.wise.dental_clinic.services.DentistService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/dentists")
+public class DentistController {
+
+    private final DentistService service;
+
+    public DentistController(DentistService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DentistDTO>> findAll() {
+        List<DentistDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DentistDTO> findById(@PathVariable Long id) {
+        DentistDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<DentistDTO> insert(@RequestBody DentistDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<DentistDTO> update(@RequestBody DentistDTO dto, @PathVariable Long id) {
+        dto = service.update(dto, id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
